7996adff41bd3447db2c6b36d2dffd108c6fc22b
Deniskaponchik <deniskaponchik@gmail.com>
2023-02-20 22:16:23 +0800

    del sortAsc

--- src/main/java/org/example/main/MergeSortInt.java
+++ src/main/java/org/example/main/MergeSortInt.java
@@ -2,46 +2,6 @@ package org.example.main;
 import java.util.Arrays;
 public class MergeSortInt {
 
-    public static int[] sortAsc(int[] result) {
-        if (result.length < 2) {
-            return result;
-        } else {
-            int[] left = sortAsc(Arrays.copyOfRange(result,0, result.length/2));
-            int[] right = sortAsc(Arrays.copyOfRange(result,result.length / 2, result.length));
-            return mergeAsc(left ,right);
-        }
-    }
-    private static int[] mergeAsc(int[] left, int[] right) {
-        int i = 0, j = 0, k = 0;
-        int[] result = new int[left.length + right.length];
-        while (i < left.length && j < right.length) {
-            if (left[i] < right[j]) {
-                result[k] = left[i];
-                i++;
-            } else {
-                result[k] = right[j];
-                j++;
-            }
-            k++;
-        }
-        if (i == left.length) {
-            while (j < right.length) {
-                result[k] = right[j];
-                j++;k++;
-            }
-        }
-        if (j == right.length) {
-            while (i < left.length) {
-                result[k] = left[i];
-                i++;k++;
-            }
-        }
-        return result;
-    }
-
-
-
-
     public static int[] sort(int[] result, boolean sortType) {
         if (result.length < 2) {
             return result;
@@ -55,10 +15,7 @@ public class MergeSortInt {
         int i = 0, j = 0, k = 0;
         int[] result = new int[left.length + right.length];
 
-
-
         while (i < left.length && j < right.length) {
-
             if (sortType) {
                 if (left[i] < right[j]) {  //для прямой сортировки
                 //if (left[i] > right[j]) {  // для обратной сортировки
@@ -81,8 +38,6 @@ public class MergeSortInt {
             k++;
         }
 
-
-
         if (i == left.length) {
             while (j < right.length) {
                 result[k] = right[j];
@@ -97,11 +52,6 @@ public class MergeSortInt {
         }
         return result;
     }
-
-
-
-
-
 
 
 
